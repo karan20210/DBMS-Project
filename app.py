1484c0c2714d5f0f42ee971a7dc2aa944b21084d
Karan Baboota <karanbaboota@gmail.com>
2022-04-18 04:45:35 +0530

    redirected to login.html

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/login', methods = ['GET', 'POST'])
 def login():
-    return "Login :)"
+    return render_template('login.html')
 
 if(__name__ == "__main__"):
     app.run(debug = True)
