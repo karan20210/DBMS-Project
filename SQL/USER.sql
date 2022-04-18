ff88090d76955f51e15a819a3c13840ac88186f6
Khwaish <khwaish.rupani@gmail.com>
2022-04-18 08:41:10 +0530

    Partially corrected Customer Table

--- SQL/USER.sql
+++ SQL/USER.sql
@@ -1,4 +1,4 @@
 INSERT INTO USER VALUES('karanbaboota', 'karanb1809', 'Customer');
 INSERT INTO USER VALUES('khwaish', 'dumbu', 'Seller');
 
-select * from user;
+insert into USER (Username, Password, Type) values ('vpotten0', 'h70yra', 'Customer');
