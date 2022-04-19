b2c6703dd7bf49a76df9db8ff5d8a1a7c5ee3333
Khwaish <khwaish.rupani@gmail.com>
2022-04-19 07:25:08 +0530

    Homepage, MyProfile frontend complete

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ import yaml
 
 app = Flask(__name__)
 
-db = yaml.safe_load(open('db.yaml'))
+# db = yaml.safe_load(open('db.yaml'))
 
-app.config['MYSQL_HOST'] = db['mysql_host']
-app.config['MYSQL_USER'] = db['mysql_user']
-app.config['MYSQL_PASSWORD'] = db['mysql_password']
-app.config['MYSQL_DB'] = db['mysql_db']
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root' 
+app.config['MYSQL_PASSWORD'] = 'Khwai0902'
+app.config['MYSQL_DB'] = 'PROJECT'
 
 mysql = MySQL(app)
 
