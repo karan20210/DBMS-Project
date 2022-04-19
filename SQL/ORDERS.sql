4084ebd1c7a765f23d6c1e2ac66c88122edb4a9e
Muskan190502 <muskan20087@iiitd.ac.in>
2022-04-20 03:07:11 +0530

    image in Products left

--- SQL/ORDERS.sql
+++ SQL/ORDERS.sql
@@ -1,27 +1,20 @@
 delete from orders where Order_id = 1 or Order_id = 2 or Order_id = 3 or Order_id = 4 or Order_id = 5 or Order_id = 6;
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (1, 1, 20221203, 1,1);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (2, 1, 20221203, 2,2);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (3, 2, 20221203, 3,1);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (4, 3, 20221203, 4,1);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (5, 1, 20221203, 5,3);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (6, 2, 20221203, 6,2);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (1, 1, '2022-12-03', 1,1);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (2, 1, '2022-12-03', 2,2);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (3, 2, '2022-12-03', 3,1);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (4, 3, '2022-12-03', 4,1);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (5, 1, '2022-12-03', 5,3);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (6, 2, '2022-12-03', 6,2);
 
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (7, 1, 20221203, 7,4);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (8, 2, 20221203, 8,3);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (9, 2, 20221203, 9,4);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (10, 3, 20221203, 10,1);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (11, 3, 20221203, 11,2);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (12, 4, 20221203, 12,3);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (13, 4, 20221203, 13,4);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (7, 1, '2022-12-03', 7,4);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (8, 2, '2022-12-03', 8,3);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (9, 2, '2022-12-03', 9,4);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (10, 3, '2022-12-03', 10,1);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (11, 3, '2022-12-03', 11,2);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (12, 4, '2022-12-03', 12,3);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (13, 4, '2022-12-03', 13,4);
 
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (14, 3, 20221203, 14,3);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (15, 3, 20221203, 15,4);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (16, 5, 20221203, 16,2);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (17, 5, 20221203, 17,3);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (18, 5, 20221203, 18,4);
-
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (19, 4, 20221203, 19,1);
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (20, 4, 20221203, 20,2);
-
-
-insert into orders (Order_ID, User_ID, Date_of_Delivery, Payment_ID,Cart_ID) values (21, 5, 20221203, 21,1);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (14, 10, '2022-12-03', 14,3);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (15, 3, '2022-12-03', 15,4);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (16, 5, '2022-12-03', 16,2);
+insert into orders (Order_ID, OCustomerID, Date_of_Delivery, Payment_ID,Cart_ID) values (17, 5, '2022-12-03', 17,3);
--- SQL/RETURNS.sql
+++ SQL/RETURNS.sql
@@ -4,6 +4,6 @@ insert into Returns (Order_ID, DP_ID , Return_Status) values (14, 9, 'Picked Up'
 insert into Returns (Order_ID, DP_ID , Return_Status) values (15, 10, 'Returned');
 insert into Returns (Order_ID, DP_ID , Return_Status) values (16, 11, 'Returned');
 insert into Returns (Order_ID, DP_ID , Return_Status) values (17, 12, 'Picked Up');
-insert into Returns (Order_ID, DP_ID , Return_Status) values (18, 16, 'DISPATCHED');
-insert into Returns (Order_ID, DP_ID , Return_Status) values (19, 12, 'Picked up');
-insert into Returns (Order_ID, DP_ID , Return_Status) values (20, 13, 'Returned');
+insert into Returns (Order_ID, DP_ID , Return_Status) values (2, 16, 'DISPATCHED');
+insert into Returns (Order_ID, DP_ID , Return_Status) values (3, 12, 'Picked up');
+insert into Returns (Order_ID, DP_ID , Return_Status) values (1, 13, 'Returned');
