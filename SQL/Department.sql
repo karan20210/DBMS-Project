21f4ebec3e6cce033e748ba2cc2fc40e4212ae15
Muskan190502 <muskan20087@iiitd.ac.in>
2022-04-19 18:21:53 +0530

    All data dump done

---
+++ SQL/Department.sql
@@ -0,0 +1,7 @@
+insert into Department(Department_ID,Department_Name) values (10, 'Seller');
+insert into Department(Department_ID,Department_Name) values (20, 'Customer');
+insert into Department(Department_ID,Department_Name) values (30, 'Delivery');
+insert into Department(Department_ID,Department_Name) values (40, 'Product');
+insert into Department(Department_ID,Department_Name) values (50, 'Order');
+insert into Department(Department_ID,Department_Name) values (60, 'All');
+
