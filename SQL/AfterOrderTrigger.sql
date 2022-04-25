0a9d3dc869ffde5cfe3bbbd519543d29e1369d9d
M-Gautam-R <85253321+M-Gautam-R@users.noreply.github.com>
2022-04-26 02:01:31 +0530

    Update AfterOrderTrigger.sql

--- SQL/AfterOrderTrigger.sql
+++ SQL/AfterOrderTrigger.sql
@@ -4,7 +4,7 @@ for each row
 begin 
 	set @OID = new.order_id;
 	set @DateOrder = new.date_of_order;
-    select DP_ID into @dpid from Delivery_person where DP_ID=2;
+    select DP_ID into @dpid from Delivery_person order by rand() limit 1;
 	insert into deliveries(Order_ID,DP_ID,Order_Status,DateofDelivery) values(@OID,@dpid,'On the way','20220404');
 end#
 delimiter ;
