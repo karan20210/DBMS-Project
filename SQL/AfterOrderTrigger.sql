afbe62552871b125802d55fa16943d666b426089
M-Gautam-R <85253321+M-Gautam-R@users.noreply.github.com>
2022-04-27 01:06:07 +0530

    Update AfterOrderTrigger.sql

--- SQL/AfterOrderTrigger.sql
+++ SQL/AfterOrderTrigger.sql
@@ -5,6 +5,7 @@ begin
 	set @OID = new.order_id;
 	set @DateOrder = new.date_of_order;
     select DP_ID into @dpid from Delivery_person order by rand() limit 1;
+    set @DDate = date_add(@DateOrder,Interval 5 day);
 	insert into deliveries(Order_ID,DP_ID,Order_Status,DateofDelivery) values(@OID,@dpid,'On the way','20220404');
 end#
 delimiter ;
